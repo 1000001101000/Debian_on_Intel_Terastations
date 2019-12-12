2503249511866b7d8c12c95564cf3ccc8b01f9a3
1000001101000 <38921757+1000001101000@users.noreply.github.com>
2019-12-12 15:12:26 -0600

    Update update_check.sh

--- .github/scripts/update_check.sh
+++ .github/scripts/update_check.sh
@@ -4,10 +4,13 @@ distros="Stretch Buster"
 
 for distro in $distros
 do
-  curl http://ftp.nl.debian.org/debian/dists/${distro,,}/main/installer-amd64/current/images/netboot/mini.iso > /tmp/latest.txt
-  diff /tmp/latest.txt installer-image/$distro/build/last_build.txt
+  curl http://ftp.nl.debian.org/debian/dists/${distro,,}/main/installer-amd64/current/images/netboot/mini.iso 2>/dev/null > /tmp/latest.txt
+  diff /tmp/latest.txt installer-image/$distro/build/last_build.txt 2>/dev/null
   if [ $? -ne 0 ]; then
     echo "trigger build of $distro images"
+  else
+    cd installer-image/$distro/build/
+    ./generate_images.sh
   fi
 
 done
