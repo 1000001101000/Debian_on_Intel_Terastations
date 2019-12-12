07715e79390160705e183ae08bd021af15b6d270
1000001101000 <38921757+1000001101000@users.noreply.github.com>
2019-12-12 14:56:07 -0600

    Update update_check.sh

--- .github/scripts/update_check.sh
+++ .github/scripts/update_check.sh
@@ -6,5 +6,9 @@ for distro in $distros
 do
   curl http://ftp.nl.debian.org/debian/dists/${distro,,}/main/installer-amd64/current/images/netboot/mini.iso > /tmp/latest.txt
   diff /tmp/latest.txt installer-image/$distro/build/last_build.txt
+  if [ $? -ne 0 ]; then
+    echo "trigger build of $distro images"
+  fi
 
 done
+exit 0
